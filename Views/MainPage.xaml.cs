dbf71dbbbb65fbe03db49983f956fe6ea194f961
Gustavo <tavo198704@gmail.com>
2024-02-14 21:15:30 -0600

    Add project files.

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace PM2E1507673
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListaSitios_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
